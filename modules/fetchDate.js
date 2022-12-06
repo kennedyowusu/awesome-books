da1e079a60c8652a616995efac7b41b98a6313e6
Kennedy Owusu <kennedyowusu@gmail.com>
2022-12-06 21:17:35 +0000

    refine

--- modules/fetchDate.js
+++
@@ -1,9 +0,0 @@
-import { DateTime } from 'luxon';
-
-const fetchDateAndTime = () => {
-  const date = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
-
-  return date;
-};
-
-export default fetchDateAndTime;
