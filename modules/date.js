2084cae4cac0e0e760be367e2e225c6a581cc5c5
Kennedy Owusu <kennedyowusu@gmail.com>
2022-12-07 03:57:02 +0000

    refine

--- modules/date.js
+++ modules/date.js
@@ -1,9 +1,8 @@
 import { DateTime } from '../node_modules/luxon/build/global/luxon.min.js';
 
 const getDateAndTime = () => {
-  const dateAndTime = DateTime.now();
-  const date = dateAndTime.toLocaleString(DateTime.DATE_MED);
-  const time = dateAndTime.toLocaleString(DateTime.TIME_SIMPLE);
+  const date = DateTime.now().toLocaleString(DateTime.DATE_MED);
+  const time = DateTime.now().toLocaleString(DateTime.TIME_SIMPLE);
   return `${date} ${time}`;
 };
 
