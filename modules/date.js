6dfff9d6817abce5daab6d323219ad6033592e0e
Kennedy Owusu <kennedyowusu@gmail.com>
2022-12-07 05:01:41 +0000

    remove comments from code

--- modules/date.js
+++ modules/date.js
@@ -1,13 +1,5 @@
 // import { DateTime } from '../node_modules/luxon/build/global/luxon.min.js';
 
-// const getDateAndTime = () => {
-//   const date = DateTime.now().toLocaleString(DateTime.DATE_MED);
-//   const time = DateTime.now().toLocaleString(DateTime.TIME_SIMPLE);
-//   return `${date} ${time}`;
-// };
-
-// export default getDateAndTime;
-
 const fetchDateAndTime = document.querySelector('.date');
 setInterval(() => {
   fetchDateAndTime.innerHTML = `${Date().slice(0, 24)}`;
